2968cc3152987bad8f78ef59abff1a39f24f1a84
assgJones <bjones@allsouls.qld.edu.au>
2013-05-27 08:42:50 +1000

    Create Commenting.py

---
+++ Commenting.py
@@ -0,0 +1,13 @@
+''' The first way of commenting a file or function is to put in a comment string using a tripple appostrophe
+
+the first line is kind of a general description, then leave a line and write more, you don't need to leave a line after the first blank line if you don't want to, these comments are part of python, and pop up to help people using your code.'''
+
+def someFunction(input):
+    '''This function takes an input and does nothing
+    
+    This is an example of a functional comment'''
+    pass
+    #the hash character can be used to make a comment on a single line.
+    
+print(somefunction(someinput))
+##fileout.write(something) #double hashes are sometimes used to camment out code which you may have used to test something
