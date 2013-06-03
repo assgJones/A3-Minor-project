840100f9853931b9e8c4e55803df7a7ebf26ae47
Grimley517 <ba.jones@me.com>
2013-06-03 21:45:04 +1000

    added class map for team and speaker to store scores and variables
    added classes fro team and speaker.

---
+++ WindowsApplication2/Speaker.vb
@@ -0,0 +1,44 @@
+﻿
+Public Class Speaker
+
+    Public Property Score As Integer
+        Get
+
+        End Get
+        Set(ByVal value As Integer)
+
+        End Set
+    End Property
+
+    Public Property Time As Integer
+        Get
+
+        End Get
+        Set(ByVal value As Integer)
+
+        End Set
+    End Property
+
+    Public Property Name As String
+        Get
+
+        End Get
+        Set(ByVal value As String)
+
+        End Set
+    End Property
+
+    Public Property position As Integer
+        Get
+
+        End Get
+        Set(ByVal value As Integer)
+
+        End Set
+    End Property
+
+    Public Sub doScore()
+
+    End Sub
+
+End Class
---
+++ WindowsApplication2/Team.vb
@@ -0,0 +1,31 @@
+﻿
+Public Class Team
+
+    Public Property Speaker As Speaker
+        Get
+
+        End Get
+        Set(ByVal value As Speaker)
+
+        End Set
+    End Property
+
+    Public Property Score As Integer
+        Get
+
+        End Get
+        Set(ByVal value As Integer)
+
+        End Set
+    End Property
+
+    Public Property View As Boolean
+        Get
+
+        End Get
+        Set(ByVal value As Boolean)
+
+        End Set
+    End Property
+
+End Class
